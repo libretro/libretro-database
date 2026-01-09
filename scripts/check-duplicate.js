7c5a0d8188af0eaa5882a9ff1939256e47c13a8d
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2026-01-09 13:50:28 +0800

    check-duplicate

--- scripts/check-duplicate.js
+++ scripts/check-duplicate.js
@@ -6,29 +6,24 @@ for await (const datFile of glob("**/*.dat")) {
   console.log(datFile);
   const datContent = await readFile(datFile, "utf8");
   const dat = parse(datContent);
-  const crcMap = {};
-  const serialMap = {};
+  const crcSerialMap = {};
   for (const game of dat) {
-    if (game.$class !== "game" || game.$entries?.length !== 1 || game.$entries?.[0].$class !== 'rom') {
+    if (
+      game.$class !== "game" ||
+      game.$entries?.length !== 1 ||
+      game.$entries?.[0].$class !== "rom"
+    ) {
       continue;
     }
     const crc = game.$entries?.[0].crc;
-    const serial = game.$entries?.[0].serial;
-    if (crc) {
-      crcMap[crc] = (crcMap[crc] || 0) + 1;
-    }
-    if (serial) {
-      serialMap[serial] = (serialMap[serial] || 0) + 1;
-    }
+    const serial = game.$entries?.[0].serial || game.serial;
+    // if both crc and serial are the same, mark as duplicate
+    const key = `${crc || ""}-${serial || ""}`;
+    crcSerialMap[key] = (crcSerialMap[key] || 0) + 1;
   }
-  for (const [crc, count] of Object.entries(crcMap)) {
+  for (const [key, count] of Object.entries(crcSerialMap)) {
     if (count > 1) {
-      console.log(chalk.red(`    duplicate crc: ${crc} (${count})`));
-    }
-  }
-  for (const [serial, count] of Object.entries(serialMap)) {
-    if (count > 1) {
-      console.log(chalk.red(`    duplicate serial: ${serial} (${count})`));
+      console.log(chalk.red(`    duplicate: crc(${key.split("-")[0]}), serial(${key.split("-")[1]}), count(${count})`));
     }
   }
 }
